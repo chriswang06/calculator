b3f032b432506681d928b5117af81b821dd0ce09
Chris Wang <ChrisMCWang1@Gmail.com>
2023-03-07 16:38:04 -0500

    created basic operators + operator function, html boilerplate.

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(x, y){
+    return x+y;
+}
+function subtract(x, y){
+    return x-y;
+}
+function multiply(x, y){
+    return x*y;
+}
+function divide(x, y){
+    return x/y;
+}
+
+function operate(s,x,y){
+    if(s == "+"){
+        return add(x,y);
+    }
+    else if (s == "-"){
+        return subtract(x,y);
+    }
+    else if (s == "*"){
+        return multiply(x,y);
+    }
+    else if (s == "/"){
+        return divide(x,y);
+    }
+    return "error";
+}
